4ab4806c451ed5494c06eccf02d7dfd426184875
renekuhm <renekuhm2@gmail.com>
2026-04-30 21:36:21 -0300

    feat: add Torrentio and Latino Movies to default addons
    - Torrentio: popular torrent addon configurable for Spanish content
    - Latino Movies: specialized in Latin American Spanish movies/series
    - Both addons provide 720p+ quality content with Spanish audio/subtitles

--- app/src/main/java/com/despegue/tv/data/local/AddonPreferences.kt
+++ app/src/main/java/com/despegue/tv/data/local/AddonPreferences.kt
@@ -165,6 +165,8 @@ class AddonPreferences @Inject constructor(
 
     private fun getDefaultAddons(): Set<String> = setOf(
         "https://v3-cinemeta.strem.io",
-        "https://opensubtitles-v3.strem.io"
+        "https://opensubtitles-v3.strem.io",
+        "https://torrentio.strem.fun",
+        "https://latino-movies.strem.io"
     )
 }
